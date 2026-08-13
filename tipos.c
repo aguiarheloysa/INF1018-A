8c91d6eb210d01a4482065958abf6241ac783940
heloysaaguiar <aguiarheloysa195@gmail.com>
2026-08-13 13:44:20 +0000

    aula2 inicio do lab2

---
+++ tipos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//um ponteiro é um numero inteiro de 8 bytes ou 64bits
+int main(void){
+    char a;
+    a = 'A';
+    printf("a= %c, a= %d\n", a, a);
+
+    a= 70;
+    printf("a= %c, a= %d\n", a, a);
+}
+
+//revisar slide2 e fazer lab2
